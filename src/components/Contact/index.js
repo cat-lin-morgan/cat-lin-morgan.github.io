39ed5dd94eaeea7af0b57246634595d7ca665df9
cat - lin <plummorgan@gmail.com>
2021-01-06 22:02:10 -0800

    validation and styling add to component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
 
+    const [ contactState, setContactState ] = useState(
+        {
+            name: '',
+            email: '',
+            message: ''
+        }
+    );
+
+    const { name, email, message } = contactState;
+
+    const [ errorMessage, setErrorMessage ] = useState('');
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('An email is required to send me a message!')
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required stuff!`)
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
+            setContactState({ ...contactState, [e.target.name]: e.target.value })
+        }
+
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+    
     return(
         <section className='contact-wrapper'>
             <div className='contacts'>
@@ -9,27 +48,36 @@ function ContactForm() {
                     <h2 className='section-title'>Contact Me</h2>
                 </div>
                 <div className='form'>
-                    <form >
+                    <form onSubmit={handleSubmit}>
                         <div>
-                            <label htmlFor='name'>Name:</label>
-                            <input type='text' name='name' />
+                            <label htmlFor='name'>Name:</label><br />
+                            <input type='text' name='name' defaultValue={name} onBlur={handleChange}/>
                         </div>
                         <div>
-                            <label htmlFor='email'>Email:</label>
-                            <input type='email' name='email' />
+                            <label htmlFor='email'>Email:</label><br />
+                            <input type='email' name='email' defaultValue={email} onBlur={handleChange}/>
                         </div>
                         <div>
-                            <label htmlFor='message'>Message:</label>
-                            <textarea name='message' rows='5' />
+                            <label htmlFor='message'>Message:</label><br />
+                            <textarea name='message' defaultValue={message} rows='5' onBlur={handleChange}/>
                         </div>
                         <button type='submit' className='btn'>Submit <i className="fas fa-smile"></i></button>
                     </form>
+                    {
+                        errorMessage && (
+                            <div>
+                                <p >{errorMessage}</p>
+                            </div>
+                        )
+                    }
                 </div>
-                <div>
+                
+                <div className='contact-number'>
                     <p>
-                        Email :<a href="mailto:plummorgan@gmail.com"> plummorgan@gmail.com</a><br />
-                        Phone # :<a href="tel:323-713-6089"> (323) 713 - 6089</a><br />
-                        Thanks for reaching out!
+                        Email :<a className='hover' href="mailto:plummorgan@gmail.com"> plummorgan@gmail.com</a><br />
+                        Phone # :<a className='hover' href="tel:323-713-6089"> (323) 713 - 6089</a><br />
+                        Thanks for reaching out!<br />
+                        <i className="far fa-smile"></i>
                     </p>
                 </div>
             </div>
---
+++ src/utils/helpers.js
@@ -0,0 +1,5 @@
+//borrowed helper function from course
+export function validateEmail(email) {
+    var re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return re.test(String(email).toLowerCase());
+}
