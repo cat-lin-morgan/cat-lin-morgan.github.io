ba198845e19e4bfbe1b19f3e016fe34e667171e5
Cat Lin <68374992+cat-lin-morgan@users.noreply.github.com>
2024-07-19 00:53:56 -0700

    adding changes to about me page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,30 +1,42 @@
 import React from "react";
-import aboutImage from "../../assets/images/my_face.png";
+import aboutImageOne from "../../assets/images/my_face.png";
+
 
 function About() {
   return (
     <section>
       <div className="about-page-wrapper">
-        <img class="cat-face" src={aboutImage} alt="Cat's face smiling." />
-        <p>
-          As a web developer, you likely have experience designing, building, and
-          maintaining websites and web applications. You may be skilled in
-          programming languages such as HTML, CSS, JavaScript, and PHP, and have
-          experience with web development frameworks and content management
-          systems. Additionally, you may be familiar with responsive design,
-          cross-browser compatibility, and web accessibility standards. It's also
-          possible that you have experience with front-end libraries and
-          frameworks such as React.js, Angular.js, and Vue.js. 
-          
-          "I am a highly
-          motivated and skilled web developer with a passion for creating visually
-          appealing and user-friendly websites. I have experience in a variety of
-          programming languages, including HTML, CSS, JavaScript, and PHP. I am
-          well-versed in responsive design and have a strong understanding of the
-          latest web development trends and technologies. I am dedicated to
-          producing high-quality work and am always looking for new challenges and
-          opportunities to improve my skills."
-        </p>
+        <div class="first-about-paragraph">
+          <img class="cat-face" src={aboutImageOne} alt="Cat's face smiling." />
+          <p>         
+            "I am a highly motivated and skilled web developer with a passion for creating visually appealing and user-friendly websites.
+            I have experience in a variety of programming languages, including HTML, CSS, JavaScript, and PHP.
+            I am well-versed in responsive design and have a strong understanding of the
+            latest web development trends and technologies.
+            I am dedicated to producing high-quality work and am always looking for new challenges and opportunities to improve my skills."
+          </p>
+          <p>
+            "I am currently studying tattooing and old school tattoo design.
+            Along with this, I have been redeveloping my drawing skills.
+            It has been refreshing to revisit an old love and allow this new design style to influence my overall design principles."
+          </p>
+        </div>
+        <div class="second-about-paragraph">
+          {/* <img class="cat-bod" src={aboutImageTwo} alt="Cat at the Ren Faire" /> */}
+          <p>
+            "I originally attended Los Angeles Community College. I was here I took my formal art and design classes. 
+            I really enjoyed my Illustration and Printmaking classes.
+            It was here that I realized I loved design far more than fine art.
+            I think it were these moments that led to where I am today."
+          </p>
+          <p>
+            "In the summer of 2020, during the height of the Covid 2019 lockdown, I decided to spend the downtime going back to school.
+            I took a Coding Bootcamp. 
+            I wasnt sure what to except and honestly, could have flopped in the intense envirnment.
+            I found instead that I was a natural.
+            Coding and design came quite easily to me."
+          </p>
+        </div>
       </div>
     </section>
   );
